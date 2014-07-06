3458daf8fc62767041b3fdc8b6c5ecd42926f26e
Alphasquare <diaz.sergio605@gmail.com>
2014-07-06 14:06:58 -0400

    Added experimental profile page

---
+++ person.php
@@ -0,0 +1,112 @@
+<?php
+include('universal.php');
+?>
+<!DOCTYPE html>
+<html >
+    <head>
+        <link href='//cdnjs.cloudflare.com/ajax/libs/animate.css/3.0.0/animate.min.css' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Roboto:100,300' rel='stylesheet' type='text/css'>
+
+<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+    <title>Alphasquare - user</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="themes/bootstrap.css" rel="stylesheet" media="screen">
+   </head>
+    <body class="profile">
+<div class="col-md-4 col-md-offset-4 profile-zone">
+      <?php 
+if(isset($_SESSION['username']))
+{
+include 'assets/navbar-logged.php';
+} else {
+include 'assets/navbar.php';
+}
+?>
+
+       
+<?php
+//We check if the users ID is defined
+if(isset($_GET['id']))
+{
+	$id = intval($_GET['id']);
+	//We check if the user exists
+	$dn = mysqli_query($link, 'select username, email, avatar, status, id, signup_date from users where id="'.$id.'"');
+	if(mysqli_num_rows($dn)>0)
+	{
+		$dnn = mysqli_fetch_array($dn);
+		//We display the user datas
+
+?>
+
+<br>
+<?php
+$username = htmlentities($dnn['username'], ENT_QUOTES, 'UTF-8');
+?>
+<center>
+<br>
+
+<?php
+//We add a link to send a pm to the user
+
+	}
+	else
+	{
+		echo '<br><div class="alert alert-info">This user does not exist.</div>';
+	}
+}
+else
+{
+	echo '<br><div class="alert alert-info">The user ID is not defined.</div>';
+}
+?>
+
+
+    <center>
+                    <img src="<?php profilePicture($username); ?>" name="aboutme" width="140" height="140" border="0" class="img-circle"></a>
+                    <h3 class="media-heading"><?php echo $username; ?></h3>
+                    
+                        <?php 
+
+$result = mysqli_query($link, 'select officia from users where id="'.$id.'"');
+
+$badges = mysqli_fetch_assoc($result); if($badges['officia'] == 1){
+echo '&nbsp;<span class="label label-success">Verified</span>';
+} 
+$result2 = mysqli_query($link, 'select ranger from users where id="'.$id.'"');
+
+$badges2 = mysqli_fetch_assoc($result2); if($badges2['ranger'] == 1){
+echo '&nbsp;<span class="label label-info">Ranger</span>';
+} 
+?>
+
+                    </center>
+                    <hr>
+                    <center>
+                    <p class="text-left"><strong>About <?php echo $username; ?>: </strong><br>
+                        <?php echo htmlentities($dnn['status']); ?> 
+</p>
+<br>
+   <?php
+if(isset($_SESSION['username']))
+{
+echo '<a href="/create.php?username='.htmlentities($dnn['username']).'" class="btn btn-primary  btn-lg btn-block">Follow</a>';
+} else {
+$username = htmlentities($dnn['username'], ENT_QUOTES, 'UTF-8');
+echo 'To talk to '.$username.',<br><br>'; 
+echo '<a href="/" class="btn btn-primary  btn-block ">Sign in to follow me.</a>';
+}
+?>
+
+
+                    <br>
+                    </center>
+      
+
+     
+</div>
+
+ <script src="http://code.jquery.com/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
