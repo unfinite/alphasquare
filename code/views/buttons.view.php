3a8ad04abe67854bed5df1698e509b7008e03a08
Alphasquare <diaz.sergio605@gmail.com>
2014-07-05 22:27:15 -0400

    Fixed links

--- code/views/buttons.view.php
+++ code/views/buttons.view.php
@@ -2,10 +2,9 @@
 <center>
              <a class="btn btn-lg btn-success " href="<?php $this->get_link("GitHub"); ?>">
          <i class="glyphicon glyphicon-hdd pull-left"></i><span> Get it now<br><small>Latest version: 1.0b</small></span></a> &nbsp;&nbsp;<a class="btn btn-lg btn-info " href="
-<center>
 <?php $this->get_link("GitHub"); ?>">
          <i class="glyphicon glyphicon-book pull-left"></i><span> Get started<br><small>Read some docs.</small></span></a> &nbsp;&nbsp;  <a class="btn btn-lg btn-primary " href="
-<center>
+
 <?php $this->get_link("GitHub"); ?>">
          <i class="glyphicon glyphicon-fire pull-left"></i><span> Contribute<br><small>Let's create the future.</small></span></a> &nbsp;&nbsp;
            <br>
