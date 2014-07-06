465d78553a3d50d350af2633445edd69ba55149a
Alphasquare <diaz.sergio605@gmail.com>
2014-07-05 22:12:18 -0400

    Added panel change of color if new

--- resources/alerts.php
+++ resources/alerts.php
@@ -3,7 +3,7 @@ include '../universal.php';
 $req = mysqli_query($link, 'select content, seen from alerts where userid="'.$_SESSION['userid'].'" order by time desc ');
 while($row=mysqli_fetch_array($req)){
 ?>
-<div class="panel panel-default">
+<div class="panel <?php if ($row['seen'] == 1) { echo "panel-default"; } else { echo "panel-primary"; } ?>" >
 <div class="panel-body">
   <?php echo html_entity_decode($row['content']); ?>
 </div>
