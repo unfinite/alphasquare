ecd529f23006aa40f5e2a6545872ddaed52e24de
Sergio <sergiomattei@outlook.com>
2014-07-24 17:19:55 -0400

    Fix

--- resources/add_comment.php
+++ resources/add_comment.php
@@ -1,20 +1,15 @@
 <?php 
 include '../universal.php';
-
+$debid = $_GET['id'];
+$comment = $_GET['content'];
 error_reporting(E_ALL);
 if (is_numeric($_GET['id']) and isset($_GET['id']) and isset($_GET['content']) and $_GET['content'] !== '') {
-
 	  $result = mysqli_query($link, 
         "SELECT * FROM debates WHERE id='".$debid."' LIMIT 1");
-
     if(mysqli_fetch_array($result) !== false) {
-    	$debid == $_GET['id'];
-$comment == $_GET['content'];
-
 	add_comment($comment, $debid);
 	echo 'Added';
 	} else { echo 'no';}
-
 } else {echo 'No'; }
 
 ?>
