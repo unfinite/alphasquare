bf74bc23a89a49ece10c316840430a4725df5c9b
Sergio <sergiomattei@outlook.com>
2014-07-24 17:04:52 -0400

    Fixed I think

--- resources/add_comment.php
+++ resources/add_comment.php
@@ -7,7 +7,7 @@ $debid == $_GET['id'];
 $comment == $_GET['content'];
 
 	  $result = mysqli_query($link, 
-        "SELECT * FROM debates WHERE id=$debid LIMIT 1");
+        "SELECT * FROM debates WHERE id=".$debid." LIMIT 1");
 
     if(mysqli_fetch_array($result) !== false) {
 	add_comment($comment, $debid);
