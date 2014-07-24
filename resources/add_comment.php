ae97d0b420c8ad828312cba57067ee1467de84ad
Sergio <sergiomattei@outlook.com>
2014-07-24 16:59:59 -0400

    Hopefully fixed comemnting bug idc about my gramamr fuck this lol

--- resources/add_comment.php
+++ resources/add_comment.php
@@ -3,8 +3,8 @@ include '../universal.php';
 
 
 if (is_numeric($_GET['id']) and isset($_GET['id']) and isset($_GET['content'])) {
-$debid = $_GET['id'];
-$comment = $_GET['content'];
+$debid == $_GET['id'];
+$comment == $_GET['content'];
 
 	  $result = mysqli_query($link, 
         "SELECT * FROM debates WHERE id=$debid LIMIT 1");
@@ -16,8 +16,4 @@ $comment = $_GET['content'];
 
 } else {echo 'No'; }
 
-$postid = $_GET['id'];
-$comment = $_GET['content'];
-
-add_comment($comment, $postid);
 ?>
