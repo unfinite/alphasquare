0907011946005b004ef1d59ce3e00b26e4e4decf
Sergio <sergiomattei@outlook.com>
2014-07-24 18:00:19 -0400

    Fixed GET bug

--- resources/add_comment.php
+++ resources/add_comment.php
@@ -3,7 +3,7 @@ include '../universal.php';
 $debid = $_POST['id'];
 $comment = $_POST['content'];
 
-if (is_numeric($_GET['id']) and isset($_GET['id']) and isset($_GET['content']) and $_GET['content'] !== '') {
+if (is_numeric($_POST['id']) and isset($_POST['id']) and isset($_POST['content']) and $_POST['content'] !== '') {
 	  $result = mysqli_query($link, 
         "SELECT * FROM debates WHERE id='".$debid."' LIMIT 1");
     if(mysqli_fetch_array($result) !== false) {
