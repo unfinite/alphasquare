8d18787286e1b4b290af4e77b7267fcdaf9d1f82
Sergio <sergiomattei@outlook.com>
2014-07-24 18:17:47 -0400

    Fixed bug

--- resources/add_comment.php
+++ resources/add_comment.php
@@ -2,13 +2,11 @@
 include '../universal.php';
 $debid = $_POST['id'];
 $comment = $_POST['content'];
-
 if (is_numeric($_POST['id']) and isset($_POST['id']) and isset($_POST['content']) and $_POST['content'] !== '') {
 	  $result = mysqli_query($link, 
         "SELECT * FROM debates WHERE id='".$debid."' LIMIT 1");
     if(mysqli_fetch_array($result) !== false) {
 	add_comment($comment, $debid);
-	echo 'Added';
 	header("Location: ../dashboard");
 	} else { echo 'no'; }
 } else {echo 'No'; }
