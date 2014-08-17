79d1adaeac48c81704116d1481723b0f7f04d979
Nathan Johnson <nathancj224@gmail.com>
2014-08-17 18:05:01 -0400

    Added json_success function to json_helper.

--- application/helpers/json_helper.php
+++ application/helpers/json_helper.php
@@ -2,26 +2,35 @@
 
 if(!defined('BASEPATH')) die('No direct script access allowed');
 
+// Call this to output an array as JSON
+// First parameter is the array
+// Second parameter is success: true or false
 if(!function_exists('json_output')) {
-	function json_output($json = array(), $success = null, $error = null) {
+	function json_output($json = array(), $success = null) {
 		header("Content-Type: application/json");
 		if(!is_null($success)) {
 			$json['success'] = $success;
-		}
-		if(!is_null($error)) {
-			$json['error'] = $error;
 		}
 		echo json_encode($json);
 		exit;
 	}
 }
 
+// Call this to output error in JSON format
 if(!function_exists('json_error')) {
 	function json_error($error, $error_word = '') {
-		json_output(array(
+		$data = array(
 			'error_word' => $error_word,
 			'error' => $error
-		), false);
+		);
+		json_output($data, false);
+	}
+}
+
+// Call this to output JSON { "success": true }
+if(!function_exists('json_success')) {
+	function json_success() {
+		json_output(null, true);
 	}
 }
 
