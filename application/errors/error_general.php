a71e6d004210d2e3066630da847d43c2925e0291
Nathan Johnson <nathancj224@gmail.com>
2014-08-23 03:06:16 -0400

    New 404 page and general error page designs.

--- application/errors/error_general.php
+++ application/errors/error_general.php
@@ -1,62 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Error</title>
-<style type="text/css">
-
-::selection{ background-color: #E13300; color: white; }
-::moz-selection{ background-color: #E13300; color: white; }
-::webkit-selection{ background-color: #E13300; color: white; }
-
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
-
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
-
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	-webkit-box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
+<title>Error :(</title>
+<link href='http://fonts.googleapis.com/css?family=Roboto:200,300,700' rel='stylesheet' type='text/css'>
+<link href="/alphasquare/assets/css/bootstrap.min.css" rel="stylesheet" />
+<link href="/alphasquare/assets/css/error-page.css" rel="stylesheet" />
 </head>
 <body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
+  <div id="container">
+    <span class="glyphicon glyphicon-exclamation-sign"></span>
+    <h1>An Error Occurred</h1>
+    <p><?php echo $message; ?></p>
+    <p class="buttons">
+      <a href="javascript:history.back();" class="btn btn-primary">&larr; Back to Last Page</a>
+    </p>
+    <p>
+      <a href="/" class="back">Go to Homepage</a>
+    </p>
+    <footer>
+      Copyright &copy; <?=date("Y")?> Alphasquare.
+      </footer>
+  </div>
 </body>
 </html>
