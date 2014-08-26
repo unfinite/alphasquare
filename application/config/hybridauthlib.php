b6ac04fd2d6523d6d2bfc6382411134493a3aa86
Nathan Johnson <nathancj224@gmail.com>
2014-08-26 02:43:51 -0400

    Reverted the debug file change.

--- application/config/hybridauthlib.php
+++ application/config/hybridauthlib.php
@@ -42,7 +42,7 @@ $config['hybridauth'] =
 		// if you want to enable logging, set 'debug_mode' to true  then provide a writable file by the web server on "debug_file"
 		"debug_mode" => (ENVIRONMENT == 'development'),
 
-		"debug_file" => APPPATH.'logs/hybridauth.log',
+		"debug_file" => APPPATH.'/logs/hybridauth.log',
 	);
 
 
