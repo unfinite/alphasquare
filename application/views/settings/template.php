bd89dbf44fd97eb32f2fbfbee5d1453a1c3944f9
Sergio Diaz <diaz.sergio605@gmail.com>
2014-08-26 04:33:55 +0000

    Changed more wording <- SO VAGUE :D

--- application/views/settings/template.php
+++ application/views/settings/template.php
@@ -1,12 +1,12 @@
 <div class="page-header">
-  <h1>Settings &amp; Info</h1>
+  <h1>Settings & Info</h1>
 </div>
 
 <ul class="nav nav-tabs">
   <li class="<? if($tab=='account') echo 'active' ?>"><a href="account">Account</a></li>
   <li class="<? if($tab=='security') echo 'active' ?>"><a href="security">Security</a></li>
   <li class="<? if($tab=='password') echo 'active' ?>"><a href="password">Password</a></li>
-  <li class="<? if($tab=='oauth') echo 'active' ?>"><a href="oauth">OAuth</a></li>
+  <li class="<? if($tab=='oauth') echo 'active' ?>"><a href="oauth">Apps</a></li>
 </ul>
 
 <br />
