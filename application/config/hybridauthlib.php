9b5b7dd5bb44a365e754c0a7580c7fcc4d39c201
Nathan Johnson <nathancj224@gmail.com>
2014-08-26 01:57:09 -0400

    Turned on codeigniter error logging

--- application/config/hybridauthlib.php
+++ application/config/hybridauthlib.php
@@ -12,7 +12,7 @@
 $config['hybridauth'] =
 	array(
 		// set on "base_url" the relative url that point to HybridAuth Endpoint
-		'base_url' => 'dev/hauth/endpoint',
+		'base_url' => 'hauth/endpoint',
 
 		"providers" => array (
 
