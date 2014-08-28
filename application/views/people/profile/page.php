4227c10653db2ae7d12f8bd2d033a148f0ddc648
Sergio Diaz <diaz.sergio605@gmail.com>
2014-08-28 18:26:43 +0000

    Design changes

--- application/views/people/profile/page.php
+++ application/views/people/profile/page.php
@@ -11,7 +11,7 @@
           <div>
             <h2>
 
-              <?=$username?>
+              <?=htmlspecialchars($name)?>
 
               <? if( $birthday && date('m-d') == date('m-d', strtotime($birthday)) ): ?>
               <span class="label label-info" title="Born on <?=$birthday_formatted?>" data-toggle="tooltip">Happy Birthday!</span>
@@ -24,7 +24,7 @@
             </h2>
 
             <? if($name): ?>
-            <span class="text-muted" style="font-size:17px;"><?=htmlspecialchars($name)?></span>
+            <span class="text-muted" style="font-size:17px;">@<?=username?></span>
             <? endif; ?>
 
             <p><?=htmlspecialchars($tagline)?></p>
