8406e816983420cfa416c1beae9697e89daecb04
Sergio <sergiomattei@outlook.com>
2014-08-29 16:45:09 -0400

    Fixed some design bugs and issues

--- application/views/admin/ban.php
+++ application/views/admin/ban.php
@@ -2,8 +2,8 @@
 <ul class="nav nav-pills">
   <li><a href="<?=base_url('employee')?>">Welcome</a></li>
   <li class="active"><a href="<?=base_url('employee/ban')?>">Ban user</a></li>
-  <li><a href="<?=base_url('employee/notes')?>">Notes</li>
+  <li><a href="<?=base_url('employee/notes')?>">Notes</a></li>
   <li><a href="<?=base_url('employee/official')?>">Add official</a></li>
-  <li><a href="<?=base_url('employee/addstaff')?>">Add staff</a></a></li>
+  <li><a href="<?=base_url('employee/addstaff')?>">Add staff</a></li>
 </ul>
 </center>
--- application/views/admin/welcome.php
+++ application/views/admin/welcome.php
@@ -1,9 +1,15 @@
 <center>
+<div class="panel panel-default">
 <ul class="nav nav-pills">
   <li class="active"><a href="<?=base_url('employee')?>">Welcome</a></li>
   <li><a href="<?=base_url('employee/ban')?>">Ban user</a></li>
-  <li><a href="<?=base_url('employee/notes')?>">Notes</li>
+  <li><a href="<?=base_url('employee/notes')?>">Notes</a></li>
   <li><a href="<?=base_url('employee/official')?>">Add official</a></li>
-  <li><a href="<?=base_url('employee/addstaff')?>">Add staff</a></a></li>
+  <li><a href="<?=base_url('employee/addstaff')?>">Add staff</a></li>
 </ul>
 </center>
+</div>
+<br>
+<h2>Welcome to the employee panel</h2>
+<hr>
+<div class="alert alert-info">Test</div>
