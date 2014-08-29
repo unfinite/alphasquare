36752f0025ce0b802c5b412886c12dbfb7fe2f70
Sergio <sergiomattei@outlook.com>
2014-08-29 15:38:04 -0400

    Added some navigation to both the views

--- application/views/admin/ban.php
+++ application/views/admin/ban.php
@@ -1,1 +1,9 @@
-Ban user
+<center>
+<ul class="nav nav-pills">
+  <li><a href="<?=base_url('employee')?>">Welcome</a></li>
+  <li class="active"><a href="<?=base_url('employee/ban')?>">Ban user</a></li>
+  <li><a href="<?=base_url('employee/notes')?>">Notes</li>
+  <li><a href="<?=base_url('employee/official')?>">Add official</a></li>
+  <li><a href="<?=base_url('employee/addstaff')?>">Add staff</a></a></li>
+</ul>
+</center>
--- application/views/admin/welcome.php
+++ application/views/admin/welcome.php
@@ -1,1 +1,9 @@
-You are employee!
+<center>
+<ul class="nav nav-pills">
+  <li class="active"><a href="<?=base_url('employee')?>">Welcome</a></li>
+  <li><a href="<?=base_url('employee/ban')?>">Ban user</a></li>
+  <li><a href="<?=base_url('employee/notes')?>">Notes</li>
+  <li><a href="<?=base_url('employee/official')?>">Add official</a></li>
+  <li><a href="<?=base_url('employee/addstaff')?>">Add staff</a></a></li>
+</ul>
+</center>
