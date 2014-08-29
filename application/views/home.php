6b9e819132f410a2665d8deeadaa79f55333349b
Sergio Diaz <diaz.sergio605@gmail.com>
2014-08-29 03:39:33 +0000

    Changed some text

--- application/views/home.php
+++ application/views/home.php
@@ -1,6 +1,6 @@
 <section class="hero-unit" id="main-hero">
-	<h1>Debate the web.</h1>
-	<p>The web is changing. Let's take control of what it will change into. <br>Let's shape the web. One debate at a time.</p>
-	<a href="register" class="create-account" data-text-swap="Let's create one! &raquo;">Create an account</a>
+	<h1>Let's shape the web; together.</h1>
+	<p>The web is changing. Let's take control of what it will change into. <br> It's our time to shine.</p>
+	<a href="register" class="create-account" data-text-swap="Let's create one! »">Create an account</a>
 	<a href="login" class="sign-in">or sign in »</a>
 </section>
