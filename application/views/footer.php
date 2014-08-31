ef151f2a5dbed2d71038aa68a3d788ee6d0a9701
Sergio <sergiomattei@outlook.com>
2014-08-31 18:01:38 -0400

    Added Google Analytics code

--- application/views/footer.php
+++ application/views/footer.php
@@ -8,3 +8,16 @@
 		<a href="<?=base_url('about/contact')?>">Contact</a>
 	</span>
 </footer>
+
+<!-- Google Analytics code start -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-45964479-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<!-- Google Analytics code end -->
