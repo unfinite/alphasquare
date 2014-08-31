62bf93ef352ee1c435d7f049429957684a93648c
Sergio <sergiomattei@outlook.com>
2014-08-31 18:10:16 -0400

    Fixed a screwup
    Whoopsies!

--- application/views/header.php
+++ application/views/header.php
@@ -30,6 +30,20 @@ $(function() {
 });
 </script>
 
+<!-- Google Analytics code start -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-45964479-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<!-- Google Analytics code end -->
+
+
 <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
