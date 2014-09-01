106f149357ace7fdef7e43fb9dd6397b68ba9e8b
Sergio <sergiomattei@outlook.com>
2014-09-01 19:04:39 -0400

    Added a few extra links, soon with corresponding controllers and the likes

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,11 +1,13 @@
 <footer id="footer">
-	© <?=date("Y");?> Alphasquare.us.
+	© <?=date("Y");?> Alphasquare
 	<span class="links">
-		<a href="<?=base_url('about/privacy')?>">Privacy</a>
-		<a href="<?=base_url('about/terms')?>">Terms</a>
-		<a href="<?=base_url('about')?>">About</a>
-		<a href="<?=base_url('help')?>">Help</a>
-		<a href="<?=base_url('about/contact')?>">Contact</a>
+		<a href="<?=base_url('about/contribute')?>">contribute</a>
+		<a href="<?=base_url('about/privacy')?>">advertise</a>
+		<a href="<?=base_url('about/privacy')?>">privacy</a>
+		<a href="<?=base_url('about/terms')?>">terms</a>
+		<a href="<?=base_url('about')?>">about</a>
+		<a href="<?=base_url('help')?>">help</a>
+		<a href="<?=base_url('about/contact')?>">contact</a>
 	</span>
 </footer>
 
--- application/views/home.php
+++ application/views/home.php
@@ -1,5 +1,5 @@
 <section class="hero-unit" id="main-hero">
-	<h1 class="animated bounceIn">Let's shape the web; together.</h1>
+	<h1 class="animated bounceIn">alphasquare is the internet's voice</h1>
 	<p>The web is changing. Let's take control of what it will change into. <br> It's our time to shine.</p>
 	<a href="register" class="create-account" data-text-swap="Let's create one! »">Create an account</a>
 	<a href="login" class="sign-in">or sign in »</a>
