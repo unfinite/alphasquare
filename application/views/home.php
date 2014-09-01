a1cd10493491a8ea57bdca2a7fb49d6d77e5f967
Sergio <sergiomattei@outlook.com>
2014-08-31 20:46:06 -0400

    fix

--- application/views/home.php
+++ application/views/home.php
@@ -1,6 +1,6 @@
 <section class="hero-unit" id="main-hero">
 	<h1 class="animated bounceIn">Let's shape the web; together.</h1>
 	<p>The web is changing. Let's take control of what it will change into. <br> It's our time to shine.</p>
-	<a href="register" class="create-account animated bounce" data-text-swap="Let's create one! »">Create an account</a>
+	<a href="register" class="create-account" data-text-swap="Let's create one! »">Create an account</a>
 	<a href="login" class="sign-in">or sign in »</a>
 </section>
