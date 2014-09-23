d79912d8ee748c7ea1d038fb5860dca6c9703459
unknown <GCI.140070@GISDGCI501ST21D.geneseeisd.local>
2014-09-23 13:21:57 -0400

    Active tab indicator

--- application/views/admin/template.php
+++ application/views/admin/template.php
@@ -1,12 +1,14 @@
 <div class="container">
 
-	<h2><?=$title?></h2>
+	<h2>Employee Panel</h2>
 	<hr />
 	<ul class="nav nav-pills">
-	  <li><a href="<?=base_url('employee')?>">Users</a></li>
-	  <li><a href="<?=base_url('employee/notes')?>">Notes</a></li>
-	  <li><a href="<?=base_url('employe/reports')?>">Reports</a></li>
+	  <li <? if($tab === 'users') echo 'active'; ?>><a href="<?=base_url('employee')?>">Users</a></li>
+	  <li <? if($tab === 'notes') echo 'active'; ?>><a href="<?=base_url('employee/notes')?>">Notes</a></li>
+	  <li <? if($tab === 'reports') echo 'active'; ?>><a href="<?=base_url('employe/reports')?>">Reports</a></li>
 	</ul>
+
+	<br />
 
 	<? $this->load->view('admin/'.$tab); ?>
 
