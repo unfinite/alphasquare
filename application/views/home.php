7b9a1cb48b70f73545bf2de451e4b80a377714f7
Sergio Diaz <diaz.sergio605@gmail.com>
2014-10-17 20:35:17 +0000

    Added contain

--- application/views/home.php
+++ application/views/home.php
@@ -1,6 +1,8 @@
+<div class="contain">
 <section class="hero-unit" id="main-hero">
 	<h1 class="animated bounceIn">alphasquare is the internet's voice</h1>
 	<p>The web is changing. Let's take control of what it will change into. <br> It's our time to shine.</p>
 	<a href="register" class="create-account" data-text-swap="Let's create one! »">Create an account</a>
 	<a href="login" class="sign-in">or sign in »</a>
 </section>
+</div>
