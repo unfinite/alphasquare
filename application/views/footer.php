d675afacc4981d593706b9f6f9e873eb9cc8185e
Sergio Diaz <diaz.sergio605@gmail.com>
2014-10-17 23:32:47 +0000

    footer.php edited online with Bitbucket

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,7 +4,7 @@
 		<a href="<?=base_url('about/privacy')?>">privacy</a>
 		<a href="<?=base_url('about/terms')?>">terms</a>
 		<a href="<?=base_url('about')?>">about</a>
-		<a href="<?=base_url('help')?>">help</a>
+		<a href="<?=base_url('about/help')?>">help</a>
 		<a href="<?=base_url('about/contact')?>">contact</a>
 	</span>
 </footer>
