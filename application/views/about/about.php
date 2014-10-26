8719416e869f828a36f007d4fced08a721cd9785
Sergio <diaz.sergio605@gmail.com>
2014-10-26 14:07:16 -0800

    Added more descriptive page

--- application/views/about/about.php
+++ application/views/about/about.php
@@ -1,4 +1,16 @@
 <div class="page-header">
-	<h1>About Us</h1>
+	<h1>What's Alphasquare</h1>
 </div>
-<p>Hi! We're Crunch D&D and we're the developers of Alphasquare.us. Alphasquare is a social debating application focused on discussing the evolution of programming and the web. Follow @Alphasquare for more!</p>
+<p>Alphasquare is a social debating application focused on discussing the evolution of programming and the web. Follow @Alphasquare for more!</p>
+<br>
+	<h2>Why join?</h2>
+	<p>Alphasquare is a community of fellow developers and designers discussing the evolution of the web and current technologies and programming practices. Believe us, it's really fun! Debates are posted daily and there's always something to discuss. So go ahead and join our awesome community of developers and designers!</p><br>
+	<h2>Why the idea?</h2>
+	<p>ou might ask why we developed this site. Because we saw that big companies and governments are monopolizing some aspects of the web. We love the web, we're passionate developers who simply want to make a change.</p><br>
+	<h2>So this is basically StackOverflow?</h2>
+	<p>Nerp. SO is another ballgame; Q&A. This site is directed at discussing vital aspects of the web and its technologies.</p>
+	<br>
+	<h2>Okay I want to join!</h2>
+	<p>Awesome! Just hit this big button here to get started:<br><center>
+		<a href="http://alphasquare.us/register" class="btn btn-success btn-block btn-lg">Create an account &raquo;</a>
+	</center></p>
