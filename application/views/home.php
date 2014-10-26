7b87eaf6a7e9343132cb7fdd90bb4a92b67b57ce
Sergio <diaz.sergio605@gmail.com>
2014-10-26 14:14:12 -0800

    Changed animation

--- application/views/home.php
+++ application/views/home.php
@@ -1,5 +1,5 @@
 <section class="hero-unit" id="main-hero">
-	<h1 class="animated flipInX">alphasquare is the internet's voice</h1>
+	<h1 class="animated fadeInDown">alphasquare is the internet's voice</h1>
   <p>Alphasquare is a <b>social debating platform</b> surrounding <b>programming</b> and the <b>evolving</b> web.<br>Let's <b>take control</b> of what the web will <b>evolve</b> into. It's <b>our time</b> to <b>shine</b>.</p>
 	<a href="register" class="create-account" data-text-swap="Let's create one! »">Create a profile »</a>
   <center><div class="hr" style="width:15%;">
