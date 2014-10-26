3027910fa4492bf10b9107c7da05cb5cda768804
Sergio <diaz.sergio605@gmail.com>
2014-10-26 14:13:00 -0800

    Emphasis on key letters for marketing

--- application/views/about/about.php
+++ application/views/about/about.php
@@ -5,7 +5,7 @@
 <p>Alphasquare is a social debating application focused on discussing the evolution of programming and the web. Follow @Alphasquare for more!</p>
 <br>
 	<h2>Why join?</h2>
-	<p>Alphasquare is a community of fellow developers and designers discussing the evolution of the web and current technologies and programming practices. Believe us, it's really fun! Debates are posted daily and there's always something to discuss. So go ahead and join our awesome community of developers and designers!</p><br>
+	<p>Alphasquare is a community of fellow developers and designers discussing the evolution of the web and current technologies and programming practices. Believe us, <b>it's really fun</b>! Debates are posted <b>daily</b> and there's always something to discuss. So go ahead and join our <b>awesome</b> community of <b>developers and designers</b>!</p><br>
 	<h2>Why the idea?</h2>
 	<p>You might ask why we developed this site. Because we saw that big companies and governments are monopolizing some aspects of the web. We love the web, we're passionate developers who simply want to make a change.</p><br>
 	<h2>So this is basically StackOverflow?</h2>
