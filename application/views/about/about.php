8a96d79c0f6e720856d4e1c58acd933b8653dcb9
Sergio <diaz.sergio605@gmail.com>
2014-10-26 14:09:22 -0800

    Design fixes

--- application/views/about/about.php
+++ application/views/about/about.php
@@ -7,7 +7,7 @@
 	<h2>Why join?</h2>
 	<p>Alphasquare is a community of fellow developers and designers discussing the evolution of the web and current technologies and programming practices. Believe us, it's really fun! Debates are posted daily and there's always something to discuss. So go ahead and join our awesome community of developers and designers!</p><br>
 	<h2>Why the idea?</h2>
-	<p>ou might ask why we developed this site. Because we saw that big companies and governments are monopolizing some aspects of the web. We love the web, we're passionate developers who simply want to make a change.</p><br>
+	<p>You might ask why we developed this site. Because we saw that big companies and governments are monopolizing some aspects of the web. We love the web, we're passionate developers who simply want to make a change.</p><br>
 	<h2>So this is basically StackOverflow?</h2>
 	<p>Nerp. SO is another ballgame; Q&A. This site is directed at discussing vital aspects of the web and its technologies.</p>
 	<br>
