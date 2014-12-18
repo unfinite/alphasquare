fe91ee9cef800e1b7f8e7d3bcca61b47b5cbbb38
Sergio <sergiomattei@outlook.com>
2014-12-18 15:32:07 -0400

    404 page design changes and bugfixes

--- application/errors/error_404.php
+++ application/errors/error_404.php
@@ -9,10 +9,10 @@
 <body>
   <div id="container">
     <span class="glyphicon glyphicon-question-sign"></span>
-    <h1>This page doesn't exist</h1>
-    <p>We're truly sorry, but we couldn't find the page you requested.</p>
+    <h1>Oh noes!</h1>
+    <p>We're truly sorry, but our magical army of unicorns and geeks couldn't find the page you requested.</p>
     <p class="buttons">
-      <form action="/alphasquare/search" method="get">
+      <form action="/search" method="get">
         <input type="text" name="q" class="form-control" placeholder="Search posts and users..." />
         <button class="btn btn-primary">Search</button>
       </form>
