cd141af3278adb2e976ddf3918253786a31c031a
Sergio <sergiomattei@outlook.com>
2014-12-21 14:35:56 -0400

    Added uid tracking for Mixpanel API

--- application/views/header.php
+++ application/views/header.php
@@ -36,6 +36,22 @@ $(function() {
 });
 </script>
 
+<?php
+
+	if(session_get("loggedin")) {
+
+		$uid = session_get("userid");
+		$points = session_get("points");
+		echo "<script>";
+		echo "var uid = ".$uid.";";
+		echo "mixpanel.identify(uid);";
+		echo 'mixpanel.people.set({"points": '.$points.'});';
+		echo "</script>";
+
+	}
+
+?>
+
 <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
