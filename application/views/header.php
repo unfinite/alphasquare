33b8fe3bf451173b31e7b04d690e8309d70895f9
Sergio <sergiomattei@outlook.com>
2014-12-24 16:06:47 -0400

    Fixed tour bug

--- application/views/header.php
+++ application/views/header.php
@@ -72,7 +72,7 @@ $(document).ready(function() {
 			},
 			{
 				placement: "bottom",
-				element: ".user:first-child",
+				element: "#posts",
 				title: "That's about it!",
 				content: "Now you should have some updates on your dash. Go ahead, interact, share debates, and talk to others! You will earn points, the more you interact, the more points. Try getting to 1k! ",
 				path: "/dashboard",
